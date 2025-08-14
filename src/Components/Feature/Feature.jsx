a5880690eb1c803fe4fbf864142b4c11dc2bf19c
dev-celo <marcelohenrique.backend@gmail.com>
2025-08-14 14:42:02 -0300

    updt: Atualizando estilo de features

--- src/Components/Feature/Feature.jsx
+++ src/Components/Feature/Feature.jsx
@@ -1,24 +1,30 @@
 /* eslint-disable react/prop-types */
 import './feature.css';
 import { useNavigate } from 'react-router-dom';
+import { FiArrowRight } from 'react-icons/fi'; // ícone de clique
 
 function Feature({ features }) {
     const navigate = useNavigate();
 
     return (
         <section id="feature" data-testid="feature" className="section-p1 p-4">
-            {
-                features.map((feature) => (
-                    <div
-                        key={feature.key}
-                        className="fe-box cursor-pointer transition duration-300 hover:scale-105"
-                        onClick={() => navigate(feature.route)}
-                    >
+            {features.map((feature) => (
+                <div
+                    key={feature.key}
+                    className={`fe-box cursor-pointer transition-transform duration-300`}
+                    onClick={() => navigate(feature.route)}
+                >
+                    <div className="fe-img-wrapper">
                         <img src={feature.imgSrc} alt={feature.altText} />
-                        <h6>{feature.altText}</h6>
                     </div>
-                ))
-            }
+                    <h6>{feature.altText}</h6>
+                    <p className="fe-description">{feature.description}</p>
+                    <div className="fe-more">
+                        <span>Ver mais</span>
+                        <FiArrowRight className="fe-arrow" />
+                    </div>
+                </div>
+            ))}
         </section>
     );
 }
